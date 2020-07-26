f84e6fbc4d5f3d7068bad02b3564ed24ee27eba4
Baris Cakir <b.cakir@amatis.nl>
2020-07-27 00:27:00 +0300

    added tests
    added tests

--- test/recordtest.js
+++ test/recordtest.js
@@ -19,7 +19,29 @@ describe("POST /records", () => {
             })
             .end((err, res) => {                  
                   res.status.should.be.equal(200);
-                  res.body.code.should.be.equal(0);                  
+                  res.body.code.should.be.equal(0);
+                  res.body.records[0].totalCount.should.be.equal(600)
+                  done();
+            });
+    });
+});
+
+describe("POST /records", () => {
+    it('should records status 200 when no data found', function(done) {
+        this.timeout(10000);    
+        chai.request(app)
+            .post('/records')
+            .send({
+                "startDate": "2017-01-26",  
+                "endDate": "2017-01-29", 
+                "minCount": 0,  
+                "maxCount": 5
+            })
+            .end((err, res) => { 
+                console.log(res.body.records[0]);                 
+                  res.status.should.be.equal(200);
+                  res.body.code.should.be.equal(0);
+                  (res.body.records[0] === 'undefined').should.be.true;
                   done();
             });
     });
